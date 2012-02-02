949c20be2a056cb9f7fa7a78cea9dd3d2696c34f
Erica Portnoy <eportnoy@princeton.edu>
2012-02-02 00:58:40 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pcard::Application.config.secret_token = 'c5003948aee34f4b3325c8981b1b764b8c198baaf13e36ee98b2753e0755141cdf7279d06211be1de1a4868f9b34066b05a25bbe6b19ee68d6b25f7d2ade4ef1'
