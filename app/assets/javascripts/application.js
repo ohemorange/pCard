67ed42fa17f63826e4802791a0484662fe025b7c
Erica Portnoy <eportnoy@princeton.edu>
2012-02-02 20:06:00 -0500

    it is so going to break

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,22 @@
 //= require twitter/bootstrap
 $(document).ready(function(){
 
+	$('.preview').click(function() {
+		var source = $(this).attr('src');
+		$('#bg').html(source);
+	});
+	
+	// shadow on mouseover
+	$('.preview').mouseover(function () {
+		$(this).addClass('.box-shadow()');
+	})
+	.mousedown(function() {
+		$(this).removeClass('.box-shadow()');
+	})
+	.mouseup(function() {
+		$(this).addClass('.box-shadow()');
+	})
+	.mouseleave(function() {
+		$(this).removeClass('.box-shadow()');
+	});
 });
